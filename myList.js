aeb5a12dfb411cde39b9a17552f8b177c4256d57
Rachit Sharma <36709071+finding-rachit@users.noreply.github.com>
2019-02-05 15:43:42 -0500

    Add files via upload

---
+++ myList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const myList = (
+  <ul> 
+    <li> React </li>
+    <li> Angular </li>
+    <li> Vue </li>
+  </ul>
+);
+
+ReactDOM.render(myList, document.getElementById('app'));
